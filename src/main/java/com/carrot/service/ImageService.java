a3351b37b1de65e1745128fc76c7b8023bad63fa
imkeunho@naver.com <imkeunho@naver.com>
2023-09-14 09:40:57 +0900

    ImageService 리팩토링 코드 최적화

---
+++ src/main/java/com/carrot/service/ImageService.java
@@ -0,0 +1,46 @@
+package com.carrot.service;
+
+import com.carrot.domain.ImageVO;
+import com.carrot.domain.ItemPostVO;
+import com.carrot.repository.ImageRepository;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ImageService {
+
+    @Autowired
+    SqlSession sqlSession;
+
+    public int insert(ImageVO vo) {
+        return sqlSession.getMapper(ImageRepository.class).insert(vo);
+    }
+
+    public List<ImageVO> selectById(int id) {
+        return sqlSession.getMapper(ImageRepository.class).selectById(id);
+    }
+
+    public List<ItemPostVO> setFirstImage(List<ItemPostVO> itemPostList) {
+        List<Integer> idArray = new ArrayList<>();
+        for (ItemPostVO itemPost : itemPostList) {
+            idArray.add(itemPost.getId());
+        }
+        List<ImageVO> imageList = sqlSession.getMapper(ImageRepository.class).selectAtFirst(idArray);
+        for (ImageVO image : imageList) {
+            int itemPostId = image.getItem_post_id();
+            for (ItemPostVO itemPost : itemPostList) {
+                if (itemPostId == itemPost.getId()) {
+                    List<ImageVO> setImage = new ArrayList<>();
+                    setImage.add(image);
+                    itemPost.setImageList(setImage);
+                }
+            }
+        }
+        return itemPostList;
+    }
+
+}
