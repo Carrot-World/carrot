7dba65a9eb0c06c7233a35dc9ce290529f140ea3
imkeunho@naver.com <imkeunho@naver.com>
2023-09-17 00:49:55 +0900

    LocationService 수정 주소값 목록 불러오기 최적화 sqlSession 최소화

--- src/main/java/com/carrot/service/LocationService.java
+++ src/main/java/com/carrot/service/LocationService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -15,6 +16,8 @@ public class LocationService {
     @Autowired
     private SqlSession sqlSession;
     private static List<String> loc1Arr;
+    private static final HashMap<String, List<String>> loc2Map = new HashMap<>();
+    private static final HashMap<String, List<String>> loc3Map = new HashMap<>();
 
     public List<String> loc1Set() {
         if (loc1Arr != null) {
@@ -29,20 +32,30 @@ public class LocationService {
         return loc1;
     }
     public List<String> loc2Set(LocationVO vo) {
+        if (loc2Map.containsKey(vo.getLoc1())) {
+            System.out.println("투 동작!");
+            return loc2Map.get(vo.getLoc1());
+        }
         List<LocationVO> locationList = sqlSession.getMapper(LocationRepository.class).selectByLoc1(vo);
         List<String> loc2 = new ArrayList<>();
         for (LocationVO locationVO : locationList) {
             loc2.add(locationVO.getLoc2());
         }
+        loc2Map.put(vo.getLoc1(), loc2);
         return loc2;
     }
 
     public List<String> loc3Set(LocationVO vo) {
+        if (loc3Map.containsKey(vo.getLoc2())) {
+            System.out.println("쓰리 동작!");
+            return loc3Map.get(vo.getLoc2());
+        }
         List<LocationVO> locationList = sqlSession.getMapper(LocationRepository.class).selectByLoc2(vo);
         List<String> loc3 = new ArrayList<>();
         for (LocationVO locationVO : locationList) {
             loc3.add(locationVO.getLoc3());
         }
+        loc3Map.put(vo.getLoc2(), loc3);
         return loc3;
     }
 }
